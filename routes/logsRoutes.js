568ad716fb6b8959d2bb32c4b18d6024550f0c5f
bhuvanesh1440 <bhuvanesh1440@gmail.com>
2025-03-04 10:23:16 +0530

    faculty token

--- routes/logsRoutes.js
+++ routes/logsRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const logsController = require("../controllers/logsController");
-
+const authenticateUser = require('../middleware/auth')
 // Route to add a new log
-router.post("/add-log", logsController.addLog); //add the log 
+router.post("/add-log",authenticateUser , logsController.addLog); //add the log 
 
 // Route to get logs by date (date format: YYYY-MM-DD)
-router.post("/getLogs", logsController.getLogsByDate); // get logs based on selected date
-router.delete("/delete-logs",logsController.deleteOldLogs)
+router.post("/getLogs",authenticateUser , logsController.getLogsByDate); // get logs based on selected date
+router.delete("/delete-logs",authenticateUser ,logsController.deleteOldLogs)
 
 module.exports = router;
