5797ba262ac219973a4e70314a3009357c9f7223
bhuvanesh1440 <bhuvanesh1440@gmail.com>
2025-03-04 11:08:14 +0530

    all API's authentication

--- routes/facultyRoutes.js
+++ routes/facultyRoutes.js
@@ -5,7 +5,7 @@ const FacultyController = require('../controllers/FacultyController')
 const authenticateUser = require("../middleware/auth");
 
 
-router.post("/add", FacultyController.createFaculty);
+router.post("/add",authenticateUser , FacultyController.createFaculty);
 router.get("/get",authenticateUser , FacultyController.getFaculty);
 router.put("/update", authenticateUser, FacultyController.updateFaculty);
 router.post("/login",FacultyController.login)
